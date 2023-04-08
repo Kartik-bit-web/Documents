c399a42f221608fa24f8b55257d24266c4691ff5
kartik <km8469879@gmail.com>
2023-04-08 06:19:58 +0000

    db_edit

--- calender_Bills/app.py
+++ calender_Bills/app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, url_for, request, redirect
 import sqlalchemy as db
+from sqlalchemy.orm import sessionmaker
 
 
 app = Flask(__name__)
+engin  = db.create_engine('sqlite:///my.db')
+connection = engin.connect()
+metadata = db.MetaData()
 
-    
+data = db.Table('info', metadata,
+                db.Column('Id', db.Integer(), primary_key= True, autoincrement=True),
+                db.Column('dated', db.String(50)),
+                db.Column('option', db.String(50)),
+                db.Column('name', db.String(50)),
+                db.Column('number', db.Integer)
+                )
 
 @app.route('/')
 def hel():
@@ -17,8 +27,22 @@ def event():
 
 @app.route('/showBooking')
 def booking():
-    #qury = db.select(engin)
-    return render_template('showBooking.html')
+    result = engin.execute('SELECT * FROM info')
+    show = result.fetchall()
+    return render_template('showBooking.html', books=show)
+
+@app.route('/editBooking')
+def editBooking():
+    return render_template('set_edit.html')
+
+@app.route('/editBooking', methods=['POST'])
+def edited():
+    edit = request.form.get('edit')
+    updateQuery = db.update(data).values(name='Kartik')
+    queryWhere = updateQuery.where(data.columns.Id == 1)
+    engin.execute(queryWhere)
+
+    return redirect('/')
 
 @app.route('/eve', methods=["POST"])
 def eve():
@@ -28,12 +52,13 @@ def eve():
     nums = request.form.get('num')
     if dat=='' and nums== '' and nm=='':
         return redirect('/event')
-    else: 
-        #x = db.insert(profile).values(dated= dat, opt= opts, name=nm, num=nums)
-        #result = conaction.execute(x)
-        #print(result)
+    else:
+        query = db.insert(data).values( dated=dat, option= opts, name= nm, number= nums)
+        engin.execute(query)
+
+        
         return redirect('/')
     
     return redirect('/event')
 
-#db.create_all(engin)
+metadata.create_all(engin)
